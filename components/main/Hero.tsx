d40561801e878c70514c31550896a325ce5fd34b
Akash Singh <manduakash@gmail.com>
2024-09-21 22:51:48 +0530

    commit

--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -7,7 +7,7 @@ const Hero = () => {
         autoPlay
         muted
         loop
-        className="rotate-180 absolute md:top-[-235px] sm:top-[0px] h-full w-full left-0 z-[-11] object-cover "
+        className="rotate-180 absolute md:top-[-235px] lg:top-[-235px] top-[-40%] h-full w-full left-0 z-[-11] object-cover "
       >
         <source src="/blackhole.webm" type="video/webm" />
       </video>
