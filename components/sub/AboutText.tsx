399147688875092c42835dd13d3df6d560c6f8e9
Akash Singh <manduakash@gmail.com>
2024-09-21 22:56:54 +0530

    commit

--- components/sub/AboutText.tsx
+++ components/sub/AboutText.tsx
@@ -24,13 +24,13 @@ const AboutText = ({setHovered} : Props) => {
         </motion.div>
           <motion.div
           variants={slideInFromLeft(0.5)}
-          className='text-[2rem] text-white font-medium mt-[10px] text-justify mb-[15px] px-[10%] dm-italic z-[20]'
+          className='md:text-[2rem] lg:text-[2rem] text-[1rem] text-white font-medium mt-[10px] md:text-justify lg:text-justify mb-[15px] px-[10%] dm-italic z-[20]'
           onMouseOver={()=>{setHovered(true)}}
           onMouseLeave={()=>{setHovered(false)}}
           >
-            I&#39;m <AnimatedText once text="Akash" el="h1" className="inline-block"/>,<br/> a full-stack developer with more than a year of hands-on experience at <span className="bebas-neue text-[2.5rem] text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">Vyoma Innovus Global</span> based in Kolkata.
-            Proficient in <span className="bebas-neue text-[2.5rem]">React <img src="/react.gif" className='text-imgs' alt="react" /> and Java</span>, I thrive on creating seamless user experiences and robust backend systems.<br/>
-            Driven by a passion for learning, I stay ahead of industry trends to bring <AnimatedText once text="fresh ideas" el="h2" className="inline-block bebas-neue text-[2.5rem] z-[200]"/> 
+            I&#39;m <AnimatedText once text="Akash" el="h1" className="inline-block"/>,<br/> a full-stack developer with more than a year of hands-on experience at <span className="bebas-neue md:text-[2.5rem] lg:text-[2.5rem] text-[1.5rem] text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">Vyoma Innovus Global</span> based in Kolkata.
+            Proficient in <span className="bebas-neue md:text-[2.5rem] lg:text-[2.5rem] text-[1.5rem]">React <img src="/react.gif" className='text-imgs' alt="react" /> and Java</span>, I thrive on creating seamless user experiences and robust backend systems.<br/>
+            Driven by a passion for learning, I stay ahead of industry trends to bring <AnimatedText once text="fresh ideas" el="h2" className="inline-block bebas-neue md:text-[2.5rem] lg:text-[2.5rem] text-[1.5rem] z-[200]"/> 
             <span><img src="/bulb.gif" className='text-imgs' alt="bulb" /></span> and <span className="bebas-neue">innovative solutions</span> <img src="/inovative-solutions.gif" className='inovative-solutions' alt="solutions" /> to every project.
           </motion.div>
         <motion.div
