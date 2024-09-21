8133e74ce32b74e161fbe8d1dd9f7ded14f38a0d
Akash Singh <manduakash@gmail.com>
2024-09-21 22:42:22 +0530

    commit

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -14,7 +14,7 @@ const HeroContent = () => {
     <motion.div
       initial="hidden"
       animate="visible"
-      className="flex flex-row items-center justify-center px-20 mt-[40px] w-full z-[20]"
+      className="flex flex-row items-center justify-center md:px-20 lg:px-20 px-5 mt-[40px] w-full z-[20]"
     >
       <div className="h-full w-full flex flex-col gap-5 justify-center m-auto text-start">
         <motion.div
@@ -29,7 +29,7 @@ const HeroContent = () => {
 
         <motion.div
           variants={slideInFromLeft(0.5)}
-          className="flex flex-col gap-6 mt-6 md:text-6xl lg:text-6xl sm:text-4xl font-bold text-white max-w-[600px] w-auto h-auto"
+          className="flex flex-col gap-6 mt-6 md:text-6xl lg:text-6xl text-3xl font-bold text-white max-w-[600px] w-auto h-auto"
         >
           <span>
             Hello there 👋<br/>
