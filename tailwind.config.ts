3d7db0f50eb87fbab463954304bd3b70eaf75c7e
Akash Singh <manduakash@gmail.com>
2024-09-22 22:10:05 +0530

    commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,7 +14,7 @@ const config: Config = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       animation: {
-        'spin-slow': 'spin 59s linear infinite',
+        'spin-slow': 'spin 120s linear infinite',
       },
     },
   },
